11fd2d20bd390198bb22d0793ef409d737946d2d
Quan <quan@gmail.com>
2022-08-11 14:43:36 +0700

    done 1108

---
+++ src/Circle.java
@@ -0,0 +1,14 @@
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+    public Circle(){}
+    public void Circle(double r){
+        this.radius = r;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public String getArea(){
+        return color;
+    }
+}
---
+++ src/demo.java
@@ -0,0 +1,8 @@
+public class demo {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle();
+        System.out.println(circle1.getRadius());
+        System.out.println(circle2.getArea());
+    }
+}
